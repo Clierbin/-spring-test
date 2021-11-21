02e825e339b30645cfac22f81ae805e82f9c03d9
zhangbin <ext.zhangbin1@jd.com>
2021-11-21 10:42:48 +0800

    bean 的lifecycle

--- src/main/java/com/zhangbin/demo/annotations/lifecycle/MyConfig.java
+++ src/main/java/com/zhangbin/demo/annotations/lifecycle/MyConfig.java
@@ -17,7 +17,7 @@ public class MyConfig {
     // 对Bean生命周期的监控
     // 1,通过@Bean 方法中 initMethod,destroyMethod 来指定对应要执行的方法
     // 2,分变实现 InitializingBean,DisposableBean 接口
-    // 3,使用@Postrouck 和 @PreDestroy 注解
+    // 3,使用@PostConstruct 和 @PreDestroy 注解
     // 4,自己写一个了类实现@BeanPostProcessor 对Bean Initialization 前后做处理
 //    @Bean(initMethod = "init" , destroyMethod = "destroy")
     @Bean
